456c2eccfce53dcadd881167b1b6d7ad32336dbc
Katerina5005 <kat51082@gmail.com>
2022-01-24 01:43:08 +0600

    incaps

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,21 +1,27 @@
-const links = document.querySelectorAll('.header-menu__item a')
-const linkCharacteristics = document.querySelector('.card-details__link-characteristics')
-const newArray = [...links, linkCharacteristics]
+const scrollFunc = () => {
+  const links = document.querySelectorAll(".header-menu__item a");
+  const linkCharacteristics = document.querySelector(
+    ".card-details__link-characteristics"
+  );
+  const newArray = [...links, linkCharacteristics];
 
-links.forEach((element) => {
+  links.forEach((element) => {
     element.addEventListener('click', (event) => {
-        event.preventDefault()
-        
-        const id = element.getAttribute('href').substring(1)
-        const section = document.getElementById(id)
+      event.preventDefault();
 
-        if (section) {
-            section.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            })
-        } else {
-            console.log('no')
-        }
-    })
-})
+      const id = element.getAttribute("href").substring(1);
+      const section = document.getElementById(id);
+
+      if (section) {
+          section.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      } else {
+        console.log("no");
+      }
+    });
+  });
+};
+
+scrollFunc()
