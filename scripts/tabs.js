67427d21d5a74ef06ccc4bb056e1bd07f837f1b5
Katerina5005 <kat51082@gmail.com>
2022-01-24 21:34:29 +0600

    new price tabs

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,56 +1,60 @@
 const tabsFunc = () => {
-  const tabs = document.querySelectorAll(".card-detail__change"),
-    tabsTitle = document.querySelector(".card-details__title"),
-    tabsPrice = document.querySelector(".card-details__price"),
-    tabsImage = document.querySelector(".card-image__item");
+    
+  const tabs = document.querySelectorAll('.card-detail__change')
+  const tabsTittle = document.querySelector('.card-details__title')
+  const tabsPrice = document.querySelector('.card-details__price')
+  const tabsImage = document.querySelector('.card__image_item')
+  const tabsMemory = document.querySelector('.description__memory')
 
   const tabsOptions = [
-    {
-      name: "Graphite",
-      memory: 128,
-      price: 60000,
-      image: "img/iPhone-graphite.webp"
-    },
-    {
-      name: "Silver",
-      memory: 256,
-      price: 65000,
-      image: "img/iPhone-silver.webp"
-    },
-    {
-      name: "Sierra Blue",
-      memory: 512,
-      price: 70000,
-      image: "img/iPhone-sierra_blue.webp"
-    },
-  ];
-
+      {
+          name: "Graphite",
+          memory: '128',
+          price: 90000,
+          image: 'img/iPhone-graphite.webp'
+      },
+      {
+          name: "Silver",
+          memory: '256',
+          price: 95000,
+          image: 'img/iPhone-silver.webp'
+      },
+      {
+          name: "Sierra Blue",
+          memory: '512',
+          price: 100000,
+          image: 'img/iPhone-sierra_blue.webp'
+      }
+  ]
   const changeContent = (index) => {
-    tabsTitle.textContent = `Смартфон Apple iPhone 13 Pro ${tabsOptions[index].memory}GB ${tabsOptions[index].name}`;
-    tabsPrice.textContent = `${tabsOptions[index].price}₽`;
+      tabsTittle.textContent = `Смартфон Apple iPhone 13 Pro ${tabsOptions[index].memory}GB ${tabsOptions[index].name}`
+      tabsPrice.textContent = `${tabsOptions[index].price}₽`
 
-    tabsImage.setAttribute("src", tabsOptions[index].image);
-  };
+      tabsImage.setAttribute('src', tabsOptions[index].image)
+
+      tabsMemory.textContent = `Встроенная память (ROM) ${tabsOptions[index].memory} ГБ`
+
+      document.title =`${tabsOptions[index].name}`
+  }
 
   const changeActiveTabs = (indexClickedTab) => {
-    tabs.forEach((tab, index) => {
-      tab.classList.remove("active");
+      tabs.forEach((tab, index) => {
+          tab.classList.remove('active')
 
-      if (index === indexClickedTab) {
-        tab.classList.add("active");
-      }
-    });
-
-    changeContent(indexClickedTab);
-  };
+          if (index === indexClickedTab) {
+              tab.classList.add('active') 
+          }
+      })
+      changeContent(indexClickedTab)
+  }
 
   tabs.forEach((tab, index) => {
-    tab.addEventListener("click", () => {
-      changeActiveTabs(index);
-    });
-  });
+      tab.addEventListener('click', () => {
+          changeActiveTabs(index)
+      })
+  })
 
-  changeContent(0);
-};
+  changeContent(0)
+}
 
-tabsFunc();
+tabsFunc()
